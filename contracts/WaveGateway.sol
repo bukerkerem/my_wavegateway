8aa5ddb2bac47bf8741c755416ccf5960d6559c4
github_bukerkerem <bukerkerem@gmail.com>
2021-12-18 21:37:18 +0300

    commit0

--- contracts/WaveGateway.sol
+++ contracts/WaveGateway.sol
@@ -3,7 +3,17 @@ pragma solidity ^0.8.4; //SPDX-License-Identifier: UNLICENSED
 import "hardhat/console.sol"; //for console logs in contract
 
 contract WaveGateway {
+    uint256 selams;
+
     constructor() { //after initialize contract, will print out
         console.log("I'm smart and contract");
     } 
+    function elsalla() public {
+        selams += 1;
+        console.log("%s has waved!", msg.sender);        
+    }
+    function toplamelsallama() public view returns (uint256) {
+        console.log("Toplam %d selam var :)", selams);
+        return selams;
+    }
 } 
